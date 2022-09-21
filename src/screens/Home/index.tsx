cbc183a51ebd6861bc1d0b9c35305494c340a58b
Hudson Tadeu <hudinvesting@gmail.com>
2022-09-21 15:07:45 -0300

    Adicionando plano de fundo

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Image } from 'react-native';
+
+import logoImg from '../../assets/background-galaxy.png';
+
+import { styles } from './styles';
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Image 
+        source={logoImg}
+        style={styles.logo}
+      />
+
+    </View>
+  );
+}
