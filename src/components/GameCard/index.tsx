8c9dc59774ad121e7458f5d8550e057dfc03b64c
Hudson Tadeu <hudinvesting@gmail.com>
2022-10-05 15:33:33 -0300

    Juntando back e front na versão mobile

--- src/components/GameCard/index.tsx
+++ src/components/GameCard/index.tsx
@@ -7,9 +7,11 @@ import { THEME } from '../../theme';
 
 export interface GameCardProps {
   id: string;
-  name: string;
-  ads: string;
-  cover: ImageSourcePropType;
+  title: string;
+  _count: {
+    ads: number;
+  };
+  bannerUrl: string;
 }
 
 interface Props extends TouchableOpacityProps {
@@ -21,7 +23,7 @@ interface Props extends TouchableOpacityProps {
     <TouchableOpacity style={styles.container} {...rest}>
       <ImageBackground 
       style={styles.cover}
-      source={data.cover}
+      source={{ uri: data.bannerUrl }}
       >
 
       <LinearGradient
@@ -29,11 +31,11 @@ interface Props extends TouchableOpacityProps {
       style={styles.footer}
       >
         <Text style={styles.name}>
-          {data.name}
+          {data.title}
         </Text>
 
         <Text style={styles.ads}>
-          {data.ads} anúcios
+          {data._count.ads} anúcios
         </Text>
       
       </LinearGradient>
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,23 @@
+import { useEffect, useState } from 'react';
 import { View, Image, FlatList } from 'react-native';
 
 import logoImg from '../../assets/logo-nlw-esports.png';
-import { GameCard } from '../../components/GameCard';
+import { GameCard, GameCardProps } from '../../components/GameCard';
 
 import { Heading } from '../../components/Heading';
 
-import { GAMES } from '../../utils/games';
 
 import { styles } from './styles';
 
 export function Home() {
+  const [games, setGames] = useState<GameCardProps[]>([]);
+
+  useEffect (() => {
+  fetch('http://192.168.2.11:3333/games')
+    .then(response => response.json())
+    .then(data => setGames(data));
+  }, []);
+
   return (
     <View style={styles.container}>
       <Image
@@ -23,7 +31,7 @@ export function Home() {
       />
 
       <FlatList 
-      data={GAMES}
+      data={games}
       keyExtractor= {item => item.id}
       renderItem={({ item }) => (
         <GameCard 
@@ -32,7 +40,8 @@ export function Home() {
       )}
       showsHorizontalScrollIndicator={false}
       horizontal
-      contentContainerStyle={styles.contentList}
+      contentContainerStyle={styles.contentList
+      }
       />
     </View>
   );
