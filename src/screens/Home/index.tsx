22c84125acc532ad37d33a98681a1796e96391b4
Hudson Tadeu <hudinvesting@gmail.com>
2022-10-07 15:02:26 -0300

    criação de interface para nova aba

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,7 +1,10 @@
 import { useEffect, useState } from 'react';
-import { View, Image, FlatList } from 'react-native';
+import { Image, FlatList } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { useNavigation } from '@react-navigation/native';
 
 import logoImg from '../../assets/logo-nlw-esports.png';
+import { Background } from '../../components/Background';
 import { GameCard, GameCardProps } from '../../components/GameCard';
 
 import { Heading } from '../../components/Heading';
@@ -12,6 +15,12 @@ import { styles } from './styles';
 export function Home() {
   const [games, setGames] = useState<GameCardProps[]>([]);
 
+  const navigation = useNavigation();
+
+  function handleOpenGame({ id, title, bannerUrl }: GameCardProps) {
+    navigation.navigate('game', { id, title, bannerUrl });
+  }
+
   useEffect (() => {
   fetch('http://192.168.2.11:3333/games')
     .then(response => response.json())
@@ -19,7 +28,8 @@ export function Home() {
   }, []);
 
   return (
-    <View style={styles.container}>
+    <Background>
+    <SafeAreaView style={styles.container}>
       <Image
         source={logoImg}
         style={styles.logo}
@@ -36,6 +46,7 @@ export function Home() {
       renderItem={({ item }) => (
         <GameCard 
         data={item}
+        onPress={() => handleOpenGame(item)}
       />
       )}
       showsHorizontalScrollIndicator={false}
@@ -43,6 +54,7 @@ export function Home() {
       contentContainerStyle={styles.contentList
       }
       />
-    </View>
+    </SafeAreaView>
+    </Background>
   );
 }
